545730b77a4ddb5cce29d4ae4f4530d71a43f3ef
hyeji <moonhyeji00@gmail.com>
2017-07-07 10:17:03 +0900

    혜지 풀받으려고

--- src/main/java/com/sist/controller/MainController.java
+++ src/main/java/com/sist/controller/MainController.java
@@ -17,189 +17,78 @@ import com.sist.ticket.dao.Buy_streamingVO;
 
 @Controller
 public class MainController {
-<<<<<<< HEAD
-   @Autowired
-   MemberDAO dao;
 
-   @Autowired
-    QuestionDAO qdao;
-   
-   @RequestMapping("main/main.do")
-   public String main_page(Model model){
-      model.addAttribute("main_jsp","default.jsp");
-      return "main/main";
-   }
-   
-   @RequestMapping("main/chart.do")
-   public String chart_page(Model model){
-      model.addAttribute("main_jsp","chart/chart.jsp");
-      return "main/main";
-   }
-   
-   @RequestMapping("main/new.do")
-   public String new_page(Model model){
-      model.addAttribute("main_jsp","new/new.jsp");
-      return "main/main";
-   }
-   
-   @RequestMapping("main/genre.do")
-   public String ganre_page(Model model){
-      model.addAttribute("main_jsp","genre/genre.jsp");
-      return "main/main";
-   }
-   
-   /*@RequestMapping("main/buy_ticket.do")
-   public String buy_ticket_page(Model model){
-      model.addAttribute("main_jsp","buy_ticket/buy_ticket.jsp");
-      return "main/main";
-   }*/
-   
-/*   @RequestMapping("main/notice.do")
-   public String notice_page(Model model){
-      model.addAttribute("main_jsp","board/board_list.jsp");
-      return "main/main";
-   }
-   */
-   @RequestMapping("main/mypage.do")
-   public String mypage_page(Model model, String nick, int id){
-      model.addAttribute("main_jsp","mypage/mypage.jsp");
-      MemberVO vo=dao.memberAllData(nick);
-
-      List<QuestionVO> qvo=qdao.questionList(id);
-
-      List<Buy_streamingVO> bsvo=dao.mypageStreamingInfo(id);
-      List<Buy_downloadVO> bdvo=dao.mypageDownloadInfo(id);
-      List<MusicVO> mvo=dao.mypagePlayList(id);
-      
-      //이용권-날짜 형식 변경(시분초 제거)
-      for(Buy_streamingVO vo2:bsvo){
-         vo2.setBuy_streaming_start(vo2.getBuy_streaming_start().substring(0,10));
-         vo2.setBuy_streaming_end(vo2.getBuy_streaming_end().substring(0,10));
-      }
-      for(Buy_downloadVO vo2:bdvo){
-         vo2.setBuy_download_start(vo2.getBuy_download_start().substring(0,10));
-         vo2.setBuy_download_end(vo2.getBuy_download_end().substring(0,10));
-      }
-      model.addAttribute("mvo", mvo);
-      model.addAttribute("vo", vo);
-      model.addAttribute("qvo",qvo);
-      model.addAttribute("bsvo",bsvo);
-      model.addAttribute("bdvo",bdvo);
-      return "main/main";
-   }
-
-   
-
-   
-   @RequestMapping("main/login.do")
-   public String login_page(Model model){
-      return "main/member/login";
-   }
-   /*
-   @RequestMapping("main/search.do")
-   public String search_page(Model model){
-      model.addAttribute("main_jsp", "search/search.jsp");
-      return "main/main";
-   }*/
-=======
 	@Autowired
 	MemberDAO dao;
 
 	@Autowired
-	 QuestionDAO qdao;
-	
+	QuestionDAO qdao;
+
 	@RequestMapping("main/main.do")
-	public String main_page(Model model){
-		model.addAttribute("main_jsp","default.jsp");
+	public String main_page(Model model) {
+		model.addAttribute("main_jsp", "default.jsp");
 		return "main/main";
 	}
-	
+
 	@RequestMapping("main/chart.do")
-	public String chart_page(Model model){
-		model.addAttribute("main_jsp","chart/chart.jsp");
+	public String chart_page(Model model) {
+		model.addAttribute("main_jsp", "chart/chart.jsp");
 		return "main/main";
 	}
-	
+
 	@RequestMapping("main/new.do")
-	public String new_page(Model model){
-		model.addAttribute("main_jsp","new/new.jsp");
+	public String new_page(Model model) {
+		model.addAttribute("main_jsp", "new/new.jsp");
 		return "main/main";
 	}
-	
+
 	@RequestMapping("main/genre.do")
-	public String ganre_page(Model model){
-		model.addAttribute("main_jsp","genre/genre.jsp");
+	public String ganre_page(Model model) {
+		model.addAttribute("main_jsp", "genre/genre.jsp");
 		return "main/main";
 	}
-	
-	/*@RequestMapping("main/buy_ticket.do")
-	public String buy_ticket_page(Model model){
-		model.addAttribute("main_jsp","buy_ticket/buy_ticket.jsp");
-		return "main/main";
-	}*/
-	
-/*	@RequestMapping("main/notice.do")
-	public String notice_page(Model model){
-		model.addAttribute("main_jsp","board/board_list.jsp");
-		return "main/main";
-	}
-	*/
+
+	/*
+	 * @RequestMapping("main/buy_ticket.do") public String buy_ticket_page(Model
+	 * model){ model.addAttribute("main_jsp","buy_ticket/buy_ticket.jsp");
+	 * return "main/main"; }
+	 */
+
+	/*
+	 * @RequestMapping("main/notice.do") public String notice_page(Model model){
+	 * model.addAttribute("main_jsp","board/board_list.jsp"); return
+	 * "main/main"; }
+	 */
 	@RequestMapping("main/mypage.do")
-	public String mypage_page(Model model, String nick, int id){
-		model.addAttribute("main_jsp","mypage/mypage.jsp");
-		MemberVO vo=dao.memberAllData(nick);
+	public String mypage_page(Model model, String nick, int id) {
+		model.addAttribute("main_jsp", "mypage/mypage.jsp");
+		MemberVO vo = dao.memberAllData(nick);
 
-		List<QuestionVO> qvo=qdao.questionList(id);
+		List<QuestionVO> qvo = qdao.questionList(id);
 
-		List<Buy_streamingVO> bsvo=dao.mypageStreamingInfo(id);
-		List<Buy_downloadVO> bdvo=dao.mypageDownloadInfo(id);
-		List<MusicVO> mvo=dao.mypagePlayList(id);
-		
-		//이용권-날짜 형식 변경(시분초 제거)
-		for(Buy_streamingVO vo2:bsvo){
-			vo2.setBuy_streaming_start(vo2.getBuy_streaming_start().substring(0,10));
-			vo2.setBuy_streaming_end(vo2.getBuy_streaming_end().substring(0,10));
+		List<Buy_streamingVO> bsvo = dao.mypageStreamingInfo(id);
+		List<Buy_downloadVO> bdvo = dao.mypageDownloadInfo(id);
+		List<MusicVO> mvo = dao.mypagePlayList(id);
+
+		// 이용권-날짜 형식 변경(시분초 제거)
+		for (Buy_streamingVO vo2 : bsvo) {
+			vo2.setBuy_streaming_start(vo2.getBuy_streaming_start().substring(0, 10));
+			vo2.setBuy_streaming_end(vo2.getBuy_streaming_end().substring(0, 10));
 		}
-		for(Buy_downloadVO vo2:bdvo){
-			vo2.setBuy_download_start(vo2.getBuy_download_start().substring(0,10));
-			vo2.setBuy_download_end(vo2.getBuy_download_end().substring(0,10));
+		for (Buy_downloadVO vo2 : bdvo) {
+			vo2.setBuy_download_start(vo2.getBuy_download_start().substring(0, 10));
+			vo2.setBuy_download_end(vo2.getBuy_download_end().substring(0, 10));
 		}
-		
 		model.addAttribute("mvo", mvo);
-<<<<<<< HEAD
-
-
-=======
->>>>>>> be3843f7f39dbf0a434cd8fdcc9cab9db53336e3
 		model.addAttribute("vo", vo);
-		model.addAttribute("qvo",qvo);
-		model.addAttribute("bsvo",bsvo);
-		model.addAttribute("bdvo",bdvo);
+		model.addAttribute("qvo", qvo);
+		model.addAttribute("bsvo", bsvo);
+		model.addAttribute("bdvo", bdvo);
 		return "main/main";
 	}
 
-	
-
-	
 	@RequestMapping("main/login.do")
-	public String login_page(Model model){
+	public String login_page(Model model) {
 		return "main/member/login";
 	}
-	/*
-	@RequestMapping("main/search.do")
-	public String search_page(Model model){
-		model.addAttribute("main_jsp", "search/search.jsp");
-		return "main/main";
-	}*/
->>>>>>> eb9942e7bdede2b8c5fccc476bfe81240bbdb5a3
-
 }
-
-
-
-
-
-
-
-
-
--- src/main/java/com/sist/controller/QuestionController.java
+++ src/main/java/com/sist/controller/QuestionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.sist.board.dao.QuestionDAO;
 import com.sist.board.dao.QuestionVO;
 
-
 @Controller
 public class QuestionController {
 
@@ -17,7 +16,7 @@ public class QuestionController {
 	private QuestionDAO dao;
 
 	@RequestMapping("main/notice_content.do")
-	public String question_Content(int question_id, String nick, int id,Model model) {
+	public String question_Content(int question_id, String nick, int id, Model model) {
 
 		QuestionVO vo = dao.questionContent(question_id);
 
@@ -29,18 +28,33 @@ public class QuestionController {
 		return "main/main";
 
 	}
+
 	@RequestMapping("main/notice_update.do")
-	public String question_Update(QuestionVO vo, String nick, int id,Model model) {
+	public String question_Update(QuestionVO vo, String nick, int id, Model model) {
 		dao.questionUpdate(vo);
 		model.addAttribute("main_jsp", "mypage.jsp");
-		return "redirect:/main/mypage.do?nick="+nick+"&id="+id;
+		return "redirect:/main/mypage.do?nick=" + nick + "&id=" + id;
 
 	}
-	
+
 	@RequestMapping("main/notice_insert.do")
-	public String question_insert(Model model){
+	public String question_insert(Model model, String nick, int id) {
 		model.addAttribute("main_jsp", "notice/notice_insert.jsp");
 		return "main/main";
-		
+
+	}
+
+	@RequestMapping("main/notice_insert_ok.do")
+	public String question_insert_ok(QuestionVO vo, String nick, int id) {
+
+		dao.questionInsert(vo);
+		return "redirect:/main/mypage.do?nick=" + nick + "&id=" + id;
+	}
+
+	@RequestMapping("main/notice_delete.do")
+	public String board_delete(int question_id, Model model) {
+		model.addAttribute("question_id", question_id);
+		dao.questionDelete(question_id);
+		return "main/notice/notice_delete";
 	}
 }
