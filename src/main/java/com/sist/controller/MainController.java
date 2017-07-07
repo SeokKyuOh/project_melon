aa23b4020ce08b9c5e00de75cc9c07734b6f3f01
sist60\sist6060 <ko4508@naver.com>
2017-07-07 15:16:08 +0900

    no message

--- src/main/java/com/sist/controller/MainController.java
+++ src/main/java/com/sist/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.sist.controller;
  
 import java.util.List;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+
 import com.sist.board.dao.QuestionDAO;
 import com.sist.board.dao.QuestionVO;
 import com.sist.chart.ChartService;
@@ -21,39 +21,7 @@ import com.sist.ticket.dao.Buy_streamingVO;
  
 @Controller
 public class MainController {
-<<<<<<< HEAD
-	@Autowired
-	private ChartService service;	
-	
-	@Autowired									//메인 앨범 이미지 때문에 추가 (7/6 오석규)
-	private GenreService genreservice;	//메인 앨범 이미지 때문에 추가 (7/6 오석규)
-	
-	@Autowired
-	MemberDAO dao;
- 
-	@Autowired
-	QuestionDAO qdao;
-	
-	List<Daily_ChartVO> daily_list;		// 상단 차트순위 때문에 추가 (7/6 오석규)
-	
-	@RequestMapping("main/main.do")
-	public String main_page(Model model){
-		//실시간 차트 불러오기
-		daily_list=service.Daily_ChartData();
-		List<GenreVO> album_main =genreservice.genreAlbumData(1);				//메인 앨범 이미지 때문에 추가 (7/6 오석규)
-		model.addAttribute("daily_list", daily_list);
-		model.addAttribute("album_main", album_main);									//메인 앨범 이미지 때문에 추가 (7/6 오석규)
-		model.addAttribute("main_jsp","default.jsp");
-		return "main/main";
-	}
-	
-	
-	/*@RequestMapping("main/buy_ticket.do")
-	public String buy_ticket_page(Model model){
-		model.addAttribute("main_jsp","buy_ticket/buy_ticket.jsp");
-		return "main/main";
-	}*/
-=======
+
    @Autowired
    private ChartService service;   
    
@@ -85,7 +53,6 @@ public class MainController {
       model.addAttribute("main_jsp","buy_ticket/buy_ticket.jsp");
       return "main/main";
    }*/
->>>>>>> 3309352a954cae609e973a81c82c97a812eeffc1
  
  
    @RequestMapping("main/mypage.do")
@@ -124,9 +91,7 @@ public class MainController {
    public String login_page(Model model){
       return "main/member/login";
    }
-<<<<<<< HEAD
-}
-=======
+
 }
  
->>>>>>> 3309352a954cae609e973a81c82c97a812eeffc1
+
