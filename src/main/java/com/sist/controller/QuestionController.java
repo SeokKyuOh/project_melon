0c509a002eebb248ba4500d7c2e8a49297f9d00f
jaesik90 <jaesik>
2017-07-07 15:02:06 +0900

    재욱 80%완성

--- src/main/java/com/sist/controller/QuestionController.java
+++ src/main/java/com/sist/controller/QuestionController.java
@@ -15,7 +15,6 @@ import com.sist.chart.Daily_ChartVO;
 @Controller
 public class QuestionController {
 
-<<<<<<< HEAD
    @Autowired
    private QuestionDAO dao;
    
@@ -25,7 +24,6 @@ public class QuestionController {
    List<Daily_ChartVO> daily_list;   //상단 차트순위 때문에 추가 (7/6 오석규)
 
    @RequestMapping("main/notice_content.do")
-
    public String question_Content(int question_id, String nick, int id,Model model) {
       
       daily_list=service.Daily_ChartData();                  // 상단 차트순위 때문에 추가 (7/6 오석규)
@@ -53,11 +51,13 @@ public class QuestionController {
 
    @RequestMapping("main/notice_insert.do")
 
-   public String question_insert(Model model){
-      
+   public String question_insert(Model model, String nick, int id) {
+      System.out.println(nick + " : " + id);
       daily_list=service.Daily_ChartData();                  // 상단 차트순위 때문에 추가 (7/6 오석규)
-      model.addAttribute("daily_list", daily_list);            // 상단 차트순위 때문에 추가 (7/6 오석규)
-      
+      model.addAttribute("daily_list", daily_list);      // 상단 차트순위 때문에 추가 (7/6 오석규)
+      model.addAttribute("nick", nick);
+      model.addAttribute("id", id);
+
       model.addAttribute("main_jsp", "notice/notice_insert.jsp");
       return "main/main";
 
@@ -65,81 +65,17 @@ public class QuestionController {
 
    @RequestMapping("main/notice_insert_ok.do")
    public String question_insert_ok(QuestionVO vo, String nick, int id) {
-
+      System.out.println(vo.getQuestion_title());
+      System.out.println("ok_nick:"+nick+",id:"+id);
       dao.questionInsert(vo);
-      return "redirect:/main/mypage.do?nick=" + nick + "&id=" + id;
+      System.out.println("insert end");
+      return "redirect:/main/mypage.do?nick="+nick +"&id="+id;
    }
 
    @RequestMapping("main/notice_delete.do")
-   public String board_delete(int question_id, Model model) {
-      model.addAttribute("question_id", question_id);
+   public String board_delete(int question_id,String nick, int id, Model model) {
+      System.out.println("del" + question_id );
       dao.questionDelete(question_id);
-      return "main/notice/notice_delete";
+      return "redirect:/main/mypage.do?nick="+nick +"&id="+id;
    }
 }
-=======
-	@Autowired
-	private QuestionDAO dao;
-	
-	@Autowired							// 상단 차트순위 때문에 추가 (7/6 오석규)
-	private ChartService service;	//상단 차트순위 때문에 추가 (7/6 오석규)
-	
-	List<Daily_ChartVO> daily_list;	//상단 차트순위 때문에 추가 (7/6 오석규)
-
-	@RequestMapping("main/notice_content.do")
-	public String question_Content(int question_id, String nick, int id,Model model) {
-		
-		daily_list=service.Daily_ChartData();						// 상단 차트순위 때문에 추가 (7/6 오석규)
-		model.addAttribute("daily_list", daily_list);				// 상단 차트순위 때문에 추가 (7/6 오석규)
-
-
-		QuestionVO vo = dao.questionContent(question_id);
-
-		model.addAttribute("vo", vo);
-		model.addAttribute("id", id);
-		model.addAttribute("nick", nick);
-		model.addAttribute("main_jsp", "notice/notice_content.jsp");
-
-		return "main/main";
-
-	}
-
-	@RequestMapping("main/notice_update.do")
-	public String question_Update(QuestionVO vo, String nick, int id, Model model) {
-		dao.questionUpdate(vo);
-		model.addAttribute("main_jsp", "mypage.jsp");
-		return "redirect:/main/mypage.do?nick=" + nick + "&id=" + id;
-
-	}
-
-	@RequestMapping("main/notice_insert.do")
-
-	public String question_insert(Model model, String nick, int id) {
-		System.out.println(nick + " : " + id);
-		daily_list=service.Daily_ChartData();						// 상단 차트순위 때문에 추가 (7/6 오석규)
-		model.addAttribute("daily_list", daily_list);		// 상단 차트순위 때문에 추가 (7/6 오석규)
-		model.addAttribute("nick", nick);
-		model.addAttribute("id", id);
-
-		model.addAttribute("main_jsp", "notice/notice_insert.jsp");
-		return "main/main";
-
-	}
-
-	@RequestMapping("main/notice_insert_ok.do")
-	public String question_insert_ok(QuestionVO vo, String nick, int id) {
-		System.out.println(vo.getQuestion_title());
-		System.out.println("ok_nick:"+nick+",id:"+id);
-		dao.questionInsert(vo);
-		System.out.println("insert end");
-		return "redirect:/main/mypage.do?nick="+nick +"&id="+id;
-	}
-
-	@RequestMapping("main/notice_delete.do")
-	public String board_delete(int question_id,String nick, int id, Model model) {
-		System.out.println("del" + question_id );
-		dao.questionDelete(question_id);
-		return "redirect:/main/mypage.do?nick="+nick +"&id="+id;
-	}
-}
->>>>>>> 1488bbb8153b34da661b967748189091037b4c63
